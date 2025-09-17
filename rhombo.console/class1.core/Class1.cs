ca53430a9c9a8b4bb0d1e0b82900f0b160b953db
JUAN-NET789 <juanduque1138734@correo.itm.edu.co>
2025-09-17 03:11:23 -0500

    figura rombo

---
+++ rhombo.console/class1.core/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace class1.core;
+
+public class RomboDrawer
+{
+    public static string DrawerRombo(int size)
+    {
+        if (size < 1 || size % 2 == 0)
+            throw new ArgumentException("The size must be an odd number greater than 0.");
+
+        var rombo = new System.Text.StringBuilder();
+        int half = size / 2;
+
+        for (int i = 0; i < size; i++)
+        {
+            int spaces = Math.Abs(half - i);
+            int characters = size - 2 * spaces;
+
+            rombo.Append(' ', spaces);
+
+            for (int j = 0; j < characters; j++)
+            {
+                if (j == 0 || j == characters - 1)
+                    rombo.Append('#');
+                else
+                    rombo.Append(' ');
+            }
+            rombo.AppendLine();
+        }
+        return rombo.ToString();
+    }
+}
+
